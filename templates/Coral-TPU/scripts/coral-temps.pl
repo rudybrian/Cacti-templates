1d7eba789a8452eb1c5df8db273b8853698af2ea
Brian Rudy <brudy@praecogito.com>
2025-03-18 09:31:09 -0700

    Updated Coral template to include trip points
    Added trip_point0_temp, trip_point1_temp and trip_point2_temp info to template.

--- templates/Coral-TPU/scripts/coral-temps.pl
+++ templates/Coral-TPU/scripts/coral-temps.pl
@@ -2,10 +2,22 @@
 #
 # Read the temps for all attached PCIe Coral TPUs
 #
+# possible arguments:
+# temp
+# trip_point0_temp
+# trip_point1_temp
+# trip_point2_temp
+# temp_poll_interval
+#
+
+unless (defined $ARGV[0]) {
+   print "Must supply parameter e.g. ./coral-temps.pl temp\n";
+   exit;
+}
 my @files = glob( '/sys/class/apex/apex_*' );
 
 foreach $apex (@files) {
-   open my $file, '<', $apex . "/temp";
+   open my $file, '<', $apex . "/" . $ARGV[0];
    my $firstLine = <$file>;
    close $file;
    print $firstLine;
